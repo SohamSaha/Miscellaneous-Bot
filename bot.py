8dddd8c82585aa1f7a48a69aab26e02f381ae92d
Soham Saha <soham.saha74@gmail.com>
2020-06-20 15:49:29 -0700

    Added a callout

--- bot.py
+++ bot.py
@@ -4,22 +4,33 @@ from discord.ext import commands, tasks
 
 
 miscFunctions = misc()
-
 client = commands.Bot(command_prefix = "?", case_insensitive=True)
 client.remove_command('help')
 
+
+#Confirming bot is ready
 @client.event
 async def on_ready():
     print ('Bot is ready.')
 
+#Error handling for if a valid command is not given
+@client.event
+async def on_command_error(ctx,error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('That is not a valid command. Type in ?help to see all the commands')
+
+
+#Flip a coin
 @client.command()
 async def coin(ctx):
     await ctx.send('```' + miscFunctions.randomCoin() + '```')
 
+#Roll a dice
 @client.command()
 async def dice(ctx):
     await ctx.send('```' + miscFunctions.randomDice() + '```')
 
+#Roast londa
 @client.command()
 async def londa(ctx):
 
@@ -31,11 +42,10 @@ async def londa(ctx):
         embed.set_image(url = value[1])
         await ctx.send(embed=embed)
 
+#Get the list of commands
 @client.command(pass_context=True)
 async def help(ctx):
-
     author = ctx.message.author
-
     embed = discord.Embed()
 
     embed.add_field(name ='!coin', value = 'Flip a coin', inline = False)
@@ -45,12 +55,14 @@ async def help(ctx):
     await author.send(embed=embed)
 
 @client.command()
+async def callout(ctx):
+    author = ctx.message.author
+    await ctx.send(author)
+    
+
+#Fucking worst command
+@client.command()
 async def quitter(ctx):
     await ctx.send('```' + str(miscFunctions.quitter()) + ' days since quitting```')
-
-@client.event
-async def on_command_error(ctx,error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send('That is not a valid command. Type in ?help to see all the commands')
 
 client.run(os.environ['DISCORD_TOKEN'])
